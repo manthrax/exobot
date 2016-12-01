a5be984bace4a419757518c6968d344e78cfffcf
Michael Schlachter <manthrax@gmail.com>
2016-11-30 19:33:19 -0800

    upd

--- nodebot.sh
+++ nodebot.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#while true
-#do
-cd ~/bot/exobot/
-sudo git pull
-sudo node index.js
-if [ $? -ne 0 ] 
-then
-	exit -1
-fi
-#	echo $?
-#done
+while true
+do
+	cd ~/bot/exobot/
+	sudo git pull
+	sudo node index.js
+	if [ $? -ne 0 ] 
+	then
+		exit -1
+	fi
+done
